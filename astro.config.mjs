547b0ad77f9a3da5718394fd6fe53e22a230f6b0
Ian <ibeluxe10@gmail.com>
2025-07-04 17:42:00 -0600

    add deploy configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 // Get the site URL from environment variable or use a default for local development
-const site = process.env.PUBLIC_SITE_URL || 'http://localhost:4321';
+const site = process.env.PUBLIC_SITE_URL || 'http://ianpalacio.github.io';
 
 export default defineConfig({
+   site: 'https://ianpalacio.github.io',
+  base: 'my-repo',
   site,
   vite: {
     plugins: [tailwindcss()],
